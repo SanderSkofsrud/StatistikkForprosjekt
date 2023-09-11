7c743a3e0b0460f14976a6bd9549e76681191d60
sande <sander@skofsrud.net>
2023-09-11 13:37:40 +0200

    Changed from excel to csv to as it was better for our program

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
-# If not already installed, install the following packages:
-# - pandas
-# - numpy
-# - matplotlib
-# - scikit-learn
-# Use the following command: pip install --user pandas matplotlib scikit-learn numpy
+"""
+This script processes a CSV file containing shoe sizes and heights to determine a linear regression.
+It then plots the data points and the regression line, saving the plot as a PDF.
+
+Authors:
+- Andrea Amundsen
+- Eline Evje
+- Elias T. Trana
+- Ina Martini
+- Malin H. Holi
+- Sander R. Skofsrud
+Date: 11-09-2023
+Version: 1.1
+"""
+
+# Required packages
+REQUIREMENTS = """
+To run this program, you need to install the following packages:
+- pandas
+- matplotlib
+- sklearn
+
+To install a package, run the following command in the terminal:
+pip install --user pandas matplotlib sklearn
+"""
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-def process_excel_data(file_path: str) -> str:
+def process_csv_data(file_path: str) -> str:
     """
-    Process an Excel file containing shoe sizes and heights to determine a linear regression.
+    Process a CSV file containing shoe sizes and heights to determine a linear regression.
     The function will plot the data points and the regression line, and save the plot as a PDF.
 
     Parameters:
-    - file_path (str): Path to the Excel file. The file is expected to have two columns labeled 'skostr' and 'hoyde'.
+    - file_path (str): Path to the CSV file. The file is expected to have two columns separated by commas.
 
     Returns:
     - str: Path to the saved PDF plot.
     """
+    # Read the CSV file.
+    data = pd.read_csv(file_path, header=None, names=['skostr', 'hoyde'])
 
-    # 1. Read the Excel file
-    data = pd.read_excel(file_path)
-
-    # 2. Perform linear regression
+    # Perform linear regression.
     X = data['skostr'].values.reshape(-1, 1)
     y = data['hoyde'].values
-
     model = LinearRegression().fit(X, y)
     a = model.intercept_
     b = model.coef_[0]
 
-    # 3. Plot the data and the regression line
+    # Plot the data and the regression line.
     plt.figure(figsize=(10, 6))
     plt.scatter(X, y, color='blue', label='Data')
     plt.plot(X, model.predict(X), color='red', label=f'Regression Line: y = {a:.2f} + {b:.2f}x')
@@ -44,7 +60,7 @@ def process_excel_data(file_path: str) -> str:
     plt.title('Relationship between Shoe Size and Height')
     plt.legend()
 
-    # 4. Save the plot as a PDF
+    # Save the plot as a PDF.
     pdf_path = "output_path.pdf"
     with PdfPages(pdf_path) as pdf:
         pdf.savefig()
@@ -53,5 +69,5 @@ def process_excel_data(file_path: str) -> str:
     return pdf_path
 
 
-# Run the function
-pdf_output_path = process_excel_data("skostr_hoyde.xlsx")
+# Run the function.
+pdf_output_path = process_csv_data("skostr_hoyde.csv")
